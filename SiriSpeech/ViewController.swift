3550127346fcc7f3695d2377cc4a215e016d3107
Wei Zhou <867571123@qq.com>
2016-12-12 17:08:43 +0800

    添加app 启动图和音频录制功能

--- SiriSpeech/ViewController.swift
+++ SiriSpeech/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
     @IBOutlet weak var microphoneButton: UIButton!
     
     private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest? // 用于处理语音识别请求，为语音识别提供音频输入
+    private var recognitionTask: SFSpeechRecognitionTask? // 返回识别请求的结果
+    private let audioEngine = AVAudioEngine() // 音频引擎
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,21 +38,100 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
             case .restricted:
                 isButtonEnabled = false
                 print("Speech recognition restricted on this device")
-            case .notDetermined: break
+            case .notDetermined:
                 isButtonEnabled = false
                 print("Speech recognition not yet authorized")
-            
             }
             
+            OperationQueue.main.addOperation {
+                self.microphoneButton.isEnabled = isButtonEnabled
+            }
         }
         
         
     }
     
     @IBAction func microphoneTapped(_ sender: AnyObject) {
-        
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            microphoneButton.isEnabled = false
+            microphoneButton.setTitle("Start Recording", for: .normal)
+        } else {
+            startRecording()
+            microphoneButton.setTitle("Stop Recording", for: .normal)
+        }
     }
 
+    func startRecording() {
+        
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+            try audioSession.setMode(AVAudioSessionModeMeasurement)
+            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode else {
+            fatalError("Audio engine has no input node")
+        }
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            var isFinal = false
+            
+            if result != nil {
+                self.textView.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.microphoneButton.isEnabled = true
+            }
+            
+            let recordingFormat = inputNode.outputFormat(forBus: 0)
+            inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+                self.recognitionRequest?.append(buffer)
+            }
+            
+            self.audioEngine.prepare()
+            
+            do {
+                try self.audioEngine.start()
+            } catch {
+                print("audioEngine couldn't start because of an error.")
+            }
+            
+            self.textView.text = "Say something, I'm listening!"
+        })
+    }
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            microphoneButton.isEnabled = true
+        } else {
+            microphoneButton.isEnabled = false
+        }
+    }
 
 }
 
