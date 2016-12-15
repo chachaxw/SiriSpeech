7f3fee9d1d9e38a1ed99bb8361574fb3fe2de9ce
zhouwei <867571123@qq.com>
2016-12-15 22:45:21 +0800

    修改 app 启动界面

--- SiriSpeech/ViewController.swift
+++ SiriSpeech/ViewController.swift
@@ -32,9 +32,9 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
         
         microphoneButton.isEnabled = false
         microphoneButton.layer.shadowOffset = CGSize(width: 0, height: 0);
-        microphoneButton.layer.shadowRadius = 4
+        microphoneButton.layer.shadowRadius = 20
         microphoneButton.layer.shadowOpacity = 0.3;
-        microphoneButton.layer.shadowColor = UIColor(red: (187/255), green: (31/255), blue: (15/255), alpha: 0.5).cgColor;
+        microphoneButton.layer.shadowColor = UIColor(red: (187/255), green: (31/255), blue: (15/255), alpha: 1.00).cgColor;
         
         speechRecognizer.delegate = self
         
